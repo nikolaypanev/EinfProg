e462fbb06b7a2cf64bc8eb0ca66700a5db315f6f
panev.nikolay <panev.nikolay@347A13.dh-karlsruhe.de>
2019-11-15 10:58:47 +0100

    added constructor & class methods to Bruch.java

--- src/blatt9/Bruch.java
+++ src/blatt9/Bruch.java
@@ -1,5 +1,44 @@
 package blatt9;
 
 public class Bruch {
+	
+	@SuppressWarnings("unused")
+	private int zaehler;
+	@SuppressWarnings("unused")
+	private int nenner;
+	
+	public Bruch(int zaehler, int nenner) {
+		this.zaehler = zaehler;
+		this.nenner = nenner;
+	}
+	
+	public Bruch() {
+		
+	}
+	
 
+	public static int ggt(int a, int b) {
+		return 0;
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
+	public static void einlesen() {
+		
+	}
+	
+	public static Bruch kuerze() {
+		return null;
+	}
+	
+	public static Bruch kehrwert() {
+		return null;
+	}
+	
+	public static Bruch neg() {
+		return null;
+	}
+	
 }
