50a7b81d734631dc74ffcdbc558707f027d889df
Nikolay Panev <panevnikolay@gmail.com>
2019-11-17 21:11:59 +0100

    Added functionality to Bruch. Tested with BruchTest

--- src/blatt9/BruchTest.java
+++ src/blatt9/BruchTest.java
@@ -4,8 +4,20 @@ public class BruchTest {
 
 	public static void main(String[] args) {
 		
-		//Testklasse für Bruchrechnen
-
+		Bruch br1 = new Bruch();
+		Bruch br2 = new Bruch();
+		
+		br1.einlesen();
+		br2.einlesen();
+		
+		System.out.println("Multiplikation: " + br1.mul(br2));
+		System.out.println("Division: " + br1.div(br2));
+		System.out.println("Addition: " + br1.add(br2));
+		System.out.println("Subtraktion: " + br1.sub(br2));
+		System.out.println("Doublewert: " + br1.wert());
+		System.out.println("Kehrwert: " + br1.kehrwert());
+		System.out.println("Negativ: " + br1.neg());
+		System.out.println("Gekuerzt: " + br1.kuerze());
 	}
 
 }
