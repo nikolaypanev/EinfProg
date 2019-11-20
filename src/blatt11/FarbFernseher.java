f5526a301e6d1006d8b5a6b34b50ca135a68b7fa
User <User@172.16.88.214>
2019-11-20 10:22:26 +0100

    added check method to FarbFernseher (not tested)

--- src/blatt11/FarbFernseher.java
+++ src/blatt11/FarbFernseher.java
@@ -2,15 +2,23 @@ package blatt11;
 
 public class FarbFernseher extends Fernseher {
 
-	public FarbFernseher(String ra, int vo, int pr) {
+	private int farbe;
+	
+	public FarbFernseher(String ra, int vo, int pr, int farbe) {
 		super(ra, vo, pr);
-		
+		this.farbe = farbe;
 	}
 	
 	public void change(String einstell, int wert) {
 		
 	}
-
 	
+	public boolean istAngenehmerAls (FarbFernseher fernseher) {
+		if (getVolume() < fernseher.getVolume() && farbe < 40 && farbe > fernseher.farbe) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 }
