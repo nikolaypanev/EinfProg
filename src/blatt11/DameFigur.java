74f085839805b183ebbbc207a3d067ce31d95529
Nikolay Panev <panevnikolay@gmail.com>
2019-11-28 14:52:44 +0100

    fixed board movement, added comments

--- src/blatt11/DameFigur.java
+++ src/blatt11/DameFigur.java
@@ -8,31 +8,28 @@ public class DameFigur extends Spielfigur {
 	public DameFigur(char x, int y, String f) {
 		super(x, y, f);
 	}
-
+	
+	//direction of movement + number of cells to move in that direction
 	public void ziehe (char richtung, int anzahl) {
 		if (richtung == '-') {
 			ziehe(anzahl, 0); 
 		} else if (richtung == '|') {
 			ziehe(0, anzahl);
 		} else if (richtung == '\\') {
-			//TODO: add left diagonal
+				ziehe(anzahl, -anzahl);
 		} else if (richtung == '/') {
-			//TODO: add right diagonal
+			ziehe(anzahl, anzahl);
 		} else {
 			System.out.println("Eingabe inkorrekt");
 		}
 	}
 	
 	public boolean trifft(DameFigur dame) {
-		if (dame.getXpos() == getXpos() && dame.getYpos() == getYpos()) {
-			return true;
-		} else {
-			return false;
-		}
+		return (dame.getXpos() == getXpos()) && (dame.getYpos() == getYpos()); //if-clause not needed, output will be a true/false value
 	}
 	
 	public String toString() {
-		return "...e Dame auf dem Feld " + getXpos() + " " + getYpos();
+		return getFarbe() + "...e Dame auf dem Feld " + getXpos() + " " + getYpos();
 	}
 	
 
